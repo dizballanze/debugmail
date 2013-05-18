0b25af380a921db35f8700dbb75f4260851bff6f
lionasp <lion.programmer@gmail.com>
2013-05-18 23:32:43 +0300

    compress

--- frontend/debugmail/settings.py
+++ frontend/debugmail/settings.py
@@ -148,7 +148,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',
 )
 
 # A sample logging configuration. The only tangible logging
