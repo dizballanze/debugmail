ff2ae445d5bf75c480927b0fa75a9b134087a34d
lionasp <lion.programmer@gmail.com>
2013-05-18 21:42:41 +0300

    add bootstrap files

--- frontend/debugmail/settings.py
+++ frontend/debugmail/settings.py
@@ -75,7 +75,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = join(PROJECT_PATH, '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -83,6 +83,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+  #  join(PROJECT_PATH, '..', 'static')
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- frontend/project/models.py
+++ frontend/project/models.py
@@ -14,7 +14,7 @@ class Letter(Document):
     subject = StringField(max_length=255, required=True)
     content_type = StringField(max_length=50, required=True)
     charset = StringField(max_length=30, required=True)
-    contents = StringField()
+    content = StringField()
     sender = StringField(max_length=255, required=True)
     to = StringField(max_length=255, required=True)
     date = DateTimeField(required=True)
