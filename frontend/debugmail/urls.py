c24f5694286965e2cd14d5f904280eebbcd10c64
dizballanze <dizballanze@gmail.com>
2013-05-19 01:45:02 +0300

    Markup

--- frontend/debugmail/urls.py
+++ frontend/debugmail/urls.py
@@ -10,10 +10,10 @@ urlpatterns = patterns('',
     url(r'^user_register/$', 'debugmail.views.user_register', name='user_register'),
     url(r'^logout/$', 'debugmail.views.logout_view', name='logout_view'),
     url(r'^projects/$', 'project.views.project_list', name='project_list'),
-    url(r'^edit_project/(?P<project_id>\w+)/$', 'project.views.edit_project', name='edit_project'),
+    url(r'^project/edit/(?P<project_id>\w+)/$', 'project.views.edit_project', name='edit_project'),
     url(r'^project/add/$', 'project.views.add_project', name='add_project'),
-    url(r'^show_project/(?P<project_id>\w+)/$', 'project.views.show_project', name='show_project'),
-    url(r'^remove_project/(?P<project_id>\w+)/$', 'project.views.remove_project', name='remove_project'),
+    url(r'^project/(?P<project_id>\w+)/$', 'project.views.show_project', name='show_project'),
+    url(r'^project/remove/(?P<project_id>\w+)/$', 'project.views.remove_project', name='remove_project'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^(?P<letter_id>\w+)/$', 'project.views.show_letter', name='show_letter')
 )
