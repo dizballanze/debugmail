54cee27a241ce3afad3fa0d21027067b7f01e969
lionasp <lion.programmer@gmail.com>
2013-05-18 21:00:27 +0300

    little refactor

--- frontend/debugmail/urls.py
+++ frontend/debugmail/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'project.views.login_view', name='login_view'),
-    url(r'^user_register/$', 'project.views.user_register', name='user_register'),
+    url(r'^$', 'debugmail.views.login_view', name='login_view'),
+    url(r'^user_register/$', 'debugmail.views.user_register', name='user_register'),
+    url(r'^user_logout/$', 'debugmail.views.logout_view', name='logout_view'),
     url(r'^project_list/$', 'project.views.project_list', name='project_list'),
-    url(r'^user_logout/$', 'project.views.logout_view', name='logout_view'),
     url(r'^edit_project/(?P<project_id>\w+)/$', 'project.views.edit_project', name='edit_project'),
     url(r'^add_project/$', 'project.views.add_project', name='add_project'),
     url(r'^show_project/(?P<project_id>\w+)/$', 'project.views.show_project', name='show_project'),
